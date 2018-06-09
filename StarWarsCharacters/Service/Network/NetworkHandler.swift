dd894f31a6477778907fd4d80d36cca5159ea82f
Nischal Hada <hadanischal@gmail.com>
2018-06-09 13:15:56 +1000

    added on NetworkService and NetworkHandler

---
+++ StarWarsCharacters/Service/Network/NetworkHandler.swift
@@ -0,0 +1,55 @@
+//
+//  NetworkHandler.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/9/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+class NetworkHandler {
+    
+    let reachability = Reachability()!
+    
+    func networkResult<T: Parceable>(completion: @escaping ((Result<[T], ErrorResult>) -> Void)) ->
+        ((Result<Data, ErrorResult>) -> Void) {
+            
+            return { dataResult in
+                
+                DispatchQueue.global(qos: .background).async(execute: {
+                    switch dataResult {
+                    case .success(let data) :
+                        ParserHelper.parse(data: data, completion: completion)
+                        break
+                    case .failure(let error) :
+                        print("Network error \(error)")
+                        completion(.failure(.network(string: "Network error " + error.localizedDescription)))
+                        break
+                    }
+                })
+                
+            }
+    }
+    
+    func networkResult<T: Parceable>(completion: @escaping ((Result<T, ErrorResult>) -> Void)) ->
+        ((Result<Data, ErrorResult>) -> Void) {
+            
+            return { dataResult in
+                print(dataResult)
+                DispatchQueue.global(qos: .background).async(execute: {
+                    switch dataResult {
+                    case .success(let data) :
+                        ParserHelper.parse(data: data, completion: completion)
+                        break
+                    case .failure(let error) :
+                        print("Network error \(error)")
+                        completion(.failure(.network(string: "Network error " + error.localizedDescription)))
+                        break
+                    }
+                })
+            }
+    }
+}
+
+
---
+++ StarWarsCharacters/Service/Network/NetworkService.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkService.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/9/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkService {
+    func loadData(urlString: String, session: URLSession = URLSession(configuration: .default), completion: @escaping (Result<Data, ErrorResult>) -> Void) -> URLSessionTask? {
+        
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.network(string: "Wrong url format")))
+            return nil
+        }
+        var request = NetworkMethod.request(method: .GET, url: url)
+        if let reachability = Reachability(), !reachability.isReachable {
+            request.cachePolicy = .returnCacheDataDontLoad
+        }
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(.network(string: "An error occured during request :" + error.localizedDescription)))
+                return
+            }
+            if let data = data {
+                print(data)
+                completion(.success(data))
+            }
+        }
+        task.resume()
+        return task
+    }
+}
