17d5aaea7da3430dc95ed01d9a17d23f51e27226
Nischal Hada <hadanischal@gmail.com>
2018-06-09 13:05:36 +1000

    added NetworkMethod

---
+++ StarWarsCharacters/Service/Network/NetworkMethod.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkMethod.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/9/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+final class NetworkMethod {
+    enum Method: String {
+        case GET,POST,PUT,DELETE,PATCH
+    }
+    
+    static func request(method: Method, url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        return request
+    }
+}
