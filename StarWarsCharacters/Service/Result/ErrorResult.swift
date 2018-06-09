51c94e0157f2d4457fe5ac2ebf0dfda3d415a144
Nischal Hada <hadanischal@gmail.com>
2018-06-09 11:42:19 +1000

    added Result for network service call

---
+++ StarWarsCharacters/Service/Result/ErrorResult.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorResult.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/9/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorResult: Error {
+    case network(string: String)
+    case parser(string: String)
+    case custom(string: String)
+}
+
---
+++ StarWarsCharacters/Service/Result/Result.swift
@@ -0,0 +1,15 @@
+//
+//  Result.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/9/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T, E: Error> {
+    case success(T)
+    case failure(E)
+}
+
