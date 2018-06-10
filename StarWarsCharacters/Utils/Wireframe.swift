3a79f726af00a32000caf43bae64c4a419196535
Nischal Hada <hadanischal@gmail.com>
2018-06-10 19:41:53 +1000

    added Wireframe for UIAlertController

---
+++ StarWarsCharacters/Utils/Wireframe.swift
@@ -0,0 +1,22 @@
+//
+//  Wireframe.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/10/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import UIKit
+
+protocol Wireframe {
+    func presentAlert(_ controller: UIViewController, title: String, message: String)
+}
+
+class DefaultWireframe: Wireframe {
+    func presentAlert(_ controller: UIViewController, title: String, message: String){
+        let alertView = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertView.addAction(UIAlertAction(title: "OK", style: .cancel) { _ in
+        })
+        controller.present(alertView, animated: true, completion: nil)
+    }
+}
