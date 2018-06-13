3116940fd936c858b7dfaf672717dc3c655b2c5e
Nischal Hada <hadanischal@gmail.com>
2018-06-13 20:12:15 +1000

    added all data case in EyeColorModel

--- StarWarsCharacters/Model/EyeColorModel.swift
+++ StarWarsCharacters/Model/EyeColorModel.swift
@@ -17,12 +17,13 @@ extension EyeColorModel {
     static func parseEyeColorArray(results: [PersonModel]) -> EyeColorModel {
         var filteredResults = [String : [PersonModel]]()
         let mapEyeColor = results.map { $0.eyeColor } as! [String]
-        let eyeColorArray = mapEyeColor.removingDuplicates()
+        var eyeColorArray = mapEyeColor.removingDuplicates()
         for eyeColor in eyeColorArray {
             let foundItems = results.filter { $0.eyeColor == eyeColor }
             filteredResults[eyeColor] = foundItems
         }
-        
+        eyeColorArray.insert("all", at: 0)
+        filteredResults["all"] = results
         let conversion = EyeColorModel(eyeColorArray: eyeColorArray, filteredResults: filteredResults)
         return conversion
     }
