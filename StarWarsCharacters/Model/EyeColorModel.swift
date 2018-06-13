03b5379bb8e2df491f080e1e77b772ea4acd2240
Nischal Hada <hadanischal@gmail.com>
2018-06-13 19:58:25 +1000

    working on EyeColorModel

---
+++ StarWarsCharacters/Model/EyeColorModel.swift
@@ -0,0 +1,15 @@
+//
+//  EyeColorModel.swift
+//  StarWarsCharacters
+//
+//  Created by Nischal Hada on 6/13/18.
+//  Copyright © 2018 NischalHada. All rights reserved.
+//
+
+import Foundation
+
+struct EyeColorModel{
+    let eyeColorArray: [String]
+    let filteredResults: [String : [PersonModel]]
+}
+
