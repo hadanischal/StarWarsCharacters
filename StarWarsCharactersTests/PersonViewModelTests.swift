c8722ff61ea51439ae10cc4aebae8ee4abb06f87
Nischal Hada <hadanischal@gmail.com>
2018-06-13 17:12:08 +1000

    PersonViewModelTests initial setup

--- StarWarsCharactersTests/PersonViewModelTests.swift
+++ StarWarsCharactersTests/PersonViewModelTests.swift
@@ -10,27 +10,28 @@ import XCTest
 @testable import StarWarsCharacters
 
 class PersonViewModelTests: XCTestCase {
+    fileprivate class MockFeedsService: CharactersRouterProtocol {
+        var feedsData: CharactersModel?
+        func fetchConverter(_ completion: @escaping ((Result<CharactersModel, ErrorResult>) -> Void)) {
+            if let data = feedsData {
+                completion(Result.success(data))
+            } else {
+                completion(Result.failure(ErrorResult.custom(string: "No converter")))
+            }
+        }
+    }
+    
+    var viewModel : PersonViewModel!
+    var dataSource : GenericDataSource<PersonModel>!
+    fileprivate var service : MockFeedsService!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
